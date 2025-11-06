86873ea9694dccc03fe047f9a7d09464d0b6d91e
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-11-06 17:24:57 +0100

    Job  7 Done.

---
+++ runtracks-cpp/jour01/job07/job07.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int inversion(int a); 
+
+int inversion(int a) {
+    string a_s = to_string(a);
+    int length = a_s.length();
+    string a_s_inverted = " ";
+
+    for (int i = 0; i < length; i++) {
+        a_s_inverted += a_s[(length-1)-i];
+    }
+
+    int b = stoi(a_s_inverted);
+    return b;
+    
+}
