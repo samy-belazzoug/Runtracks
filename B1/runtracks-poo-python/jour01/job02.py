70d31ef8887999ffb61fdb59a1ce0162b0e58fb8
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-11-14 10:36:07 +0100

    Job 02 Done

---
+++ B1/runtracks-poo-python/jour01/job02.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+
+if __name__ == "__main__":
+    addition = Operation()
+    print("Le nombre1 est : ",addition.nombre1)
+    print("Le nombre2 est : ",addition.nombre2)
