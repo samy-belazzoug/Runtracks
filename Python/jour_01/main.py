b580e7387cc895403cb7b5252359d4195c852a68
samy-belazzoug <samy.belazzoug@pm.me>
2026-07-27 18:40:19 +0200

    Job 02 done.

---
+++ Python/jour_01/main.py
@@ -0,0 +1,8 @@
+"""Job 02
+Copiez le code ci-dessous dans un fichier nommé main.py :
+➔ print(10+3)
+Assurez-vous que le résultat qui s'affiche dans le terminal soit « 13 » en
+exécutant votre programme grâce à la commande : « python3 main.py »
+"""
+
+print(10+3) # Affiche 13 dans le terminal
