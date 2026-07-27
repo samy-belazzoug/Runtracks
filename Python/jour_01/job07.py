34307c2ce5997a0c712a7986d75818cc0d887ffc
samy-belazzoug <samy.belazzoug@pm.me>
2026-07-27 18:59:41 +0200

    Job 07 done.

---
+++ Python/jour_01/job07.py
@@ -0,0 +1,9 @@
+"""Job 07
+Créez un programme qui affiche dans le terminal la somme des variables «
+num1 » et « num2 ». Assignez la valeur « 40 » à « num1 » et la valeur « 2 » à «
+num2 ».
+"""
+
+num1 = 40
+num2 = 2
+print(40+2)
