63723a5126856a1f19ab57feaf9a247aa1d5472f
samy-belazzoug <samy.belazzoug@pm.me>
2026-07-27 19:24:11 +0200

    Job 08 done.

---
+++ Python/jour_01/job08.py
@@ -0,0 +1,9 @@
+"""Job 08
+Créez un programme qui affiche dans le terminal le produit des variables «
+num1 » et « num2 ». Assignez la valeur de « 3 » à « num1 » et la valeur « 14 » à «
+num2 »
+"""
+
+num1 = 3
+num2 = 14
+print(num1 * num2)
