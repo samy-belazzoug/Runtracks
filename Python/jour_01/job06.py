cc64c25a667693555e0e514a10282a4b23871a1d
samy-belazzoug <samy.belazzoug@pm.me>
2026-07-27 18:49:52 +0200

    Job 06 done.

---
+++ Python/jour_01/job06.py
@@ -0,0 +1,7 @@
+"""Job 06
+Créez un programme qui stocke la phrase « Je suis une String » dans une
+variable nommée « ma_string », puis affichez cette variable dans le terminal.
+"""
+
+ma_string = "Je suis une String" # la variable ma_string stocke en mémoire la valeure "Je suis une String"
+print(ma_string) # Ici, on affiche la valeure de ma_string, qui est "Je suis une string"
